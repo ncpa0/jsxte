9c9f3423f1a9539bcece0312a0c26d88f1ab1106
ncpa0cpl <szymonb21@gmail.com>
2022-07-24 12:17:36 +0200

    feat: chaned nase props type to interface and added it to a global namespace

--- src/jsx/base-html-tag-props.ts
+++ src/jsx/base-html-tag-props.ts
@@ -2,103 +2,16 @@ import type { Rewrap } from "../html-parser/types";
 
 export type AttributeBool = true | false | "true" | "false";
 
-export type BaseHTMLTagProps = {
-  children?: JSX.ElementChildren;
-
-  accesskey?: string;
-  class?: string;
-  contenteditable?: AttributeBool;
-  dir?: "ltr" | "rtl" | "auto";
-  draggable?: AttributeBool | "auto";
-  hidden?: AttributeBool;
-  id?: string;
-  lang?: string;
-  onabort?: string;
-  onafterprint?: string;
-  onbeforeprint?: string;
-  onbeforeunload?: string;
-  onblur?: string;
-  oncanplay?: string;
-  oncanplaythrough?: string;
-  onchange?: string;
-  onclick?: string;
-  oncontextmenu?: string;
-  oncopy?: string;
-  oncuechange?: string;
-  oncut?: string;
-  ondblclick?: string;
-  ondrag?: string;
-  ondragend?: string;
-  ondragenter?: string;
-  ondragleave?: string;
-  ondragover?: string;
-  ondragstart?: string;
-  ondrop?: string;
-  ondurationchange?: string;
-  onemptied?: string;
-  onended?: string;
-  onerror?: string;
-  onfocus?: string;
-  onhashchange?: string;
-  oninput?: string;
-  oninvalid?: string;
-  onkeydown?: string;
-  onkeypress?: string;
-  onkeyup?: string;
-  onload?: string;
-  onloadeddata?: string;
-  onloadedmetadata?: string;
-  onloadstart?: string;
-  onmousedown?: string;
-  onmousemove?: string;
-  onmouseout?: string;
-  onmouseover?: string;
-  onmouseup?: string;
-  onmousewheel?: string;
-  onoffline?: string;
-  ononline?: string;
-  onpagehide?: string;
-  onpageshow?: string;
-  onpaste?: string;
-  onpause?: string;
-  onplay?: string;
-  onplaying?: string;
-  onpopstate?: string;
-  onprogress?: string;
-  onratechange?: string;
-  onreset?: string;
-  onresize?: string;
-  onscroll?: string;
-  onsearch?: string;
-  onseeked?: string;
-  onseeking?: string;
-  onselect?: string;
-  onstalled?: string;
-  onstorage?: string;
-  onsubmit?: string;
-  onsuspend?: string;
-  ontimeupdate?: string;
-  ontoggle?: string;
-  onunload?: string;
-  onvolumechange?: string;
-  onwaiting?: string;
-  onwheel?: string;
-  slot?: string;
-  spellcheck?: AttributeBool;
-  style?: string;
-  tabindex?: string | number;
-  title?: string;
-  translate?: "yes" | "no";
-};
-
 export type HTMLProps<T extends object = never> = Rewrap<
   ExtendBaseProps<
-    [T] extends [never] ? BaseHTMLTagProps : Partial<T> & BaseHTMLTagProps
+    [T] extends [never]
+      ? JSXTE.BaseHTMLTagProps
+      : Partial<T> & JSXTE.BaseHTMLTagProps
   >
 >;
 
 type ExtendBaseProps<P> = {
-  [K in keyof P]: TJSXExtends.AttributeAcceptedTypes extends {
+  [K in keyof P]: JSXTE.AttributeAcceptedTypes extends {
     [E in K]: infer T;
   }
     ? T | P[K]
@@ -106,7 +19,98 @@ type ExtendBaseProps<P> = {
 };
 
 declare global {
-  namespace TJSXExtends {
+  namespace JSXTE {
     interface AttributeAcceptedTypes {}
+
+    export interface BaseHTMLTagProps {
+      children?: JSX.ElementChildren;
+
+      accesskey?: string;
+      class?: string;
+      contenteditable?: AttributeBool;
+      dir?: "ltr" | "rtl" | "auto";
+      draggable?: AttributeBool | "auto";
+      hidden?: AttributeBool;
+      id?: string;
+      inert?: AttributeBool;
+      lang?: string;
+      onabort?: string;
+      onafterprint?: string;
+      onbeforeprint?: string;
+      onbeforeunload?: string;
+      onblur?: string;
+      oncanplay?: string;
+      oncanplaythrough?: string;
+      onchange?: string;
+      onclick?: string;
+      oncontextmenu?: string;
+      oncopy?: string;
+      oncuechange?: string;
+      oncut?: string;
+      ondblclick?: string;
+      ondrag?: string;
+      ondragend?: string;
+      ondragenter?: string;
+      ondragleave?: string;
+      ondragover?: string;
+      ondragstart?: string;
+      ondrop?: string;
+      ondurationchange?: string;
+      onemptied?: string;
+      onended?: string;
+      onerror?: string;
+      onfocus?: string;
+      onhashchange?: string;
+      oninput?: string;
+      oninvalid?: string;
+      onkeydown?: string;
+      onkeypress?: string;
+      onkeyup?: string;
+      onload?: string;
+      onloadeddata?: string;
+      onloadedmetadata?: string;
+      onloadstart?: string;
+      onmousedown?: string;
+      onmousemove?: string;
+      onmouseout?: string;
+      onmouseover?: string;
+      onmouseup?: string;
+      onmousewheel?: string;
+      onoffline?: string;
+      ononline?: string;
+      onpagehide?: string;
+      onpageshow?: string;
+      onpaste?: string;
+      onpause?: string;
+      onplay?: string;
+      onplaying?: string;
+      onpopstate?: string;
+      onprogress?: string;
+      onratechange?: string;
+      onreset?: string;
+      onresize?: string;
+      onscroll?: string;
+      onsearch?: string;
+      onseeked?: string;
+      onseeking?: string;
+      onselect?: string;
+      onstalled?: string;
+      onstorage?: string;
+      onsubmit?: string;
+      onsuspend?: string;
+      ontimeupdate?: string;
+      ontoggle?: string;
+      onunload?: string;
+      onvolumechange?: string;
+      onwaiting?: string;
+      onwheel?: string;
+      role?: string;
+      slot?: string;
+      spellcheck?: AttributeBool;
+      style?: string;
+      tabindex?: string | number;
+      title?: string;
+      translate?: "yes" | "no";
+    }
   }
 }
