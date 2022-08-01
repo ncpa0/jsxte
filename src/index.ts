f8884283dc83078508ebf4b8a885e4085b829e74
ncpa0cpl <sbretner@touchbistro.com>
2022-08-01 11:44:05 +0200

    refactor: build script update, disabled bundling

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+import "./utilities/array-flat-polyfill";
+
 export { renderToHtml, renderToHtmlAsync } from "./html-parser/render-to-html";
-export * from "./express";
-export {
-  renderToStringTemplateTag,
-  StringTemplateParserOptions,
-} from "./string-template-parser/render-to-string-template-tag";
+export * from "./express/index";
+export type { StringTemplateParserOptions } from "./string-template-parser/render-to-string-template-tag";
+export { renderToStringTemplateTag } from "./string-template-parser/render-to-string-template-tag";
 export type { HTMLProps } from "./jsx/base-html-tag-props";
 export * from "./jsx/jsx.types";
