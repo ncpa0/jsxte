95a55602efa15354c28a1702613c664ffbfac9ee
ncpa0cpl <sbretner@touchbistro.com>
2023-03-10 15:33:14 +0100

    ci: updated publish workflow to bump the pacakge version using the gh-bot

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,14 +7,12 @@ set -o errexit
 issemver=$(./scripts/check-semver.sh -t "$TAG_NAME")
 currenttag=$(npm pkg get version)
 
-git config user.name github-actions
-git config user.email github-actions@github.com
-
 if [ "$issemver" -eq "1" ]; then
     if ! [ "$currenttag" = "\"$TAG_NAME\"" ]; then
         echo "Branch tag is different than packge.json version. Updating package.json version to $TAG_NAME"
         npm version "$TAG_NAME"
-        git push --no-verify
+        echo "Requesting GithubBot to bump the package.json version on the remote"
+        node ./scripts/bump-remote.mjs "$TAG_NAME"
     fi
     echo "Publishing to npm"
     npm publish
