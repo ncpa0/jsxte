b1ef1b957980446ba8b492548171f154c403fe52
ncpa0cpl <sbretner@touchbistro.com>
2023-03-10 16:13:03 +0100

    fix: bump-remote script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,7 +10,7 @@ currenttag=$(npm pkg get version)
 if [ "$issemver" -eq "1" ]; then
     if ! [ "$currenttag" = "\"$TAG_NAME\"" ]; then
         echo "Branch tag is different than packge.json version. Updating package.json version to $TAG_NAME"
-        npm version "$TAG_NAME"
+        npm version "$TAG_NAME" --no-git-tag-version
         echo "Requesting GithubBot to bump the package.json version on the remote"
         node ./scripts/bump-remote.mjs "$TAG_NAME"
     fi
