1d44ec137d8c55a0209e0b0c2f0a7c93ebcf85f6
ncpa0cpl <32438334+ncpa0cpl@users.noreply.github.com>
2023-12-05 12:21:47 +0100

    fix: incorrect function name exported from jsx-dev-runtime

--- src/jsx/jsx-dev-runtime.ts
+++ src/jsx/jsx-dev-runtime.ts
@@ -8,4 +8,14 @@ import {
   jsxs,
 } from "./jsx-runtime";
 
-export { Fragment, _Fragment, _jsx, _jsxs, createElement, jsx, jsxs };
+export {
+  Fragment,
+  _Fragment,
+  _jsx,
+  _jsxs,
+  createElement,
+  jsx,
+  jsxs,
+  jsx as jsxDEV,
+  jsxs as jsxsDEV,
+};
